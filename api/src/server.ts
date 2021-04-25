cdb97614bc0098e98b52a27f6b3212b2cd7834cc
joaoMarinho94 <joaopaulim94@gmail.com>
2021-04-24 23:27:47 -0300

    Added setup websocket

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,32 +1,5 @@
-import express from "express";
-import { createServer } from "http";
-import path from "path";
-import { Server, Socket } from "socket.io";
+import { http } from "./app";
+// import './websocket/client';
+// import './websocket/admin';
 
-import { routes } from "./routes";
-
-import "./database";
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.set("views", path.join(__dirname, "..", "public"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "ejs");
-
-app.get("/pages/client", (req, res) => {
-  return res.render("html/client.html");
-});
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on("connection", (socket: Socket) => {
-  console.log("Se conectou", socket.id);
-});
-
-app.use(express.json());
-
-app.use(routes);
-
-app.listen(3333, () => console.log("Server is running on port 3333"));
+http.listen(3333, () => console.log("Server started on 3333"));
